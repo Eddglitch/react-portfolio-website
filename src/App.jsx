7105a105c8deda0236f4d8fca08a17db1e00969d
Eddglitch <eduardtelv@gmail.com>
2023-06-05 01:04:10 -0600

    agregando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import Header from '././Components/header/header'
+import React from "react";
+
+// Importando Componentes 
+import Header from "././Components/header/header";
+import About from "./Components/about/about";
+import Nav from "./Components/nav/nav";
+import Experience from "./Components/experience/experience";
+import Portfolio from "./Components/portfolio/portfolio";
+import Services from "./Components/services/services";
+import Contact from "./Components/contact/contact";
+import Testimonials from "./Components/testimonials/testimonials";
+import Footer from "./Components/footer/footer";
+
+
 const App = () => {
   return (
     <>
-        <Header/>
+
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/Components/nav/nav.jsx
+++ src/Components/nav/nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./"
+import "./nav.css"
 
 const Nav = () => {
   return (
