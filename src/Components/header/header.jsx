4c6400e941ae4214d82bdc42b04c451b895c6359
Eddglitch <eduardtelv@gmail.com>
2023-06-05 05:03:28 -0600

    avance 05/junio/2023

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import "./header.css";
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h1> Hello I'm</h1>
+        <h1>Téllez Valverde Eduardo</h1>
+        <h5 className="text-light">
+          Front-End Web Developer
+        </h5>
+        <CTA/>
+        
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>
+          Scroll Down
+        </a> 
+
+      </div>
+    </header>
   )
 }
 
